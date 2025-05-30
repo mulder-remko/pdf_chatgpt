39d21bc85d75a4b55a8c535cc9fe878f60959eff
mulder-remko <samulder@remko.de>
2025-05-30 20:16:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import openai
-openai.api_key = st.secrets["OPENAI_API_KEY"]
+# openai.api_key = st.secrets["OPENAI_API_KEY"]
 import os
-# openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.getenv("OPENAI_API_KEY")
 # wenn semantic_data.pkl fehlt,automatisch index bauen
 if not os.path.exists("semantic_data.pkl"):
     from semantic_index import build_index  # du musst build_index in semantic_index.py exportieren
